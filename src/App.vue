138d95d4c04babaedeede26f76b1cc44de4086ae
Ilya <ilya.orlov123@mail.ru>
2025-06-26 15:04:25 +1000

    test fix

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <h1>{{ t('navigation.nav_about') }}</h1>
-  <button @click="switchLanguage">Сменить</button>
+  <h1>{{ 'f' }}</h1>
+  <button>Сменить</button>
 </template>
 
 <script setup lang="js">
-import { onMounted } from 'vue'
-import { useI18n } from 'vue-i18n'
-const { t, locale } = useI18n()
-console.log(t)
+// import { onMounted } from 'vue'
+// import { useI18n } from 'vue-i18n'
+// const { t, locale } = useI18n()
+// console.log(t)
 
-const switchLanguage = () => {
-  locale.value = locale.value === 'ru' ? 'en' : 'ru'
-  localStorage.setItem('language', locale.value)
-}
+// const switchLanguage = () => {
+//   locale.value = locale.value === 'ru' ? 'en' : 'ru'
+//   localStorage.setItem('language', locale.value)
+// }
 
-onMounted(() => {
-  locale.value = localStorage.getItem('language')
-})
+// onMounted(() => {
+//   locale.value = localStorage.getItem('language')
+// })
 </script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { i18n } from '@/localization/index'
+// import { i18n } from '@/localization/index'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(i18n)
+// app.use(i18n)
 
 app.mount('#app')
