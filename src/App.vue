e339eca3fdb8389aefd8ed3bc8fe67cd83b97da7
Ilya <ilya.orlov123@mail.ru>
2025-06-26 15:08:25 +1000

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <h1>{{ 'f' }}</h1>
+  <h1>{{ t('navigation.nav_about') }}</h1>
   <button>Сменить</button>
 </template>
 
 <script setup lang="js">
 // import { onMounted } from 'vue'
-// import { useI18n } from 'vue-i18n'
-// const { t, locale } = useI18n()
-// console.log(t)
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+console.log(t)
 
 // const switchLanguage = () => {
 //   locale.value = locale.value === 'ru' ? 'en' : 'ru'
