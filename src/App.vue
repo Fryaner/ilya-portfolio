3a957959f8d74c419294f881f9998471e8cf7f58
Ilya <ilya.orlov123@mail.ru>
2025-06-30 05:26:31 +1000

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <headerComponent />
   <coverMain />
   <titleComponent>
-    <template v-slot:title>ff</template>
+    <template v-slot:title>О нас</template>
+  </titleComponent>
+  <titleComponent>
+    <template v-slot:title>Проекты</template>
+    <template v-slot:subtitle>Множество</template>
   </titleComponent>
 </template>
 
 <script setup lang="js">
 import headerComponent from './components/header-main.vue'
 import coverMain from './components/cover-main.vue'
-import titleComponent from './components/ui/title-component.vue'
+import titleComponent from './components/ui/title-UI.vue'
 </script>
