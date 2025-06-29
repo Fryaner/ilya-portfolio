3f04bf07046b44ce8fc11d071bc3d5c6dd65f4be
Ilya <ilya.orlov123@mail.ru>
2025-06-30 05:22:20 +1000

    add: ДОбавил компонет загаловка

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <headerComponent />
   <coverMain />
+  <titleComponent>
+    <template v-slot:title>ff</template>
+  </titleComponent>
 </template>
 
 <script setup lang="js">
 import headerComponent from './components/header-main.vue'
 import coverMain from './components/cover-main.vue'
+import titleComponent from './components/ui/title-component.vue'
 </script>
---
+++ src/components/ui/title-UI.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="title">
+    <h2>
+      <slot name="title"></slot>
+    </h2>
+    <h3><slot name="subtitle"></slot></h3>
+    <div class="line"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  h2 {
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 40px;
+    margin-bottom: 16px;
+    color: var(--text-primary);
+  }
+
+  p {
+    color: var(--text-secondary);
+    margin-bottom: 48px;
+    line-height: 24px;
+  }
+}
+.line {
+  width: 80px;
+  height: 4px;
+  background: linear-gradient(90deg, transparent, var(--brand-primary), transparent);
+}
+</style>
